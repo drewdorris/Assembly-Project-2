fc23ae652e0e01b9f447880b21b62eb30d4f3a4b
Drew <contact@dorrisd.com>
2021-10-20 19:23:09 -0400

    more comments, rename variables

--- project.c
+++ project.c
@@ -5,12 +5,17 @@
 #define MAX_FILE_LENGTH 2000 // maximum amnt of characters in the file
 
 int main(int argc, char *argv[]) {
+	// stores all input
 	char all[MAX_FILE_LENGTH];
 	
+	// bool: tracks whether the previous char was a newline
 	short afterNewLine = 0;
+	// bool: tracks whether we are ignoring the current line we are on
 	short ignoreLine = 0;
+	// stores current char
 	char ch;
-	int i = 0;
+	// used as index to place chars in appropriate place in all[]
+	int current = 0;
 	// https://stackoverflow.com/questions/15883568/reading-from-stdin
 	// iterate each char in input
 	while(read(STDIN_FILENO, &ch, 1) > 0)
@@ -30,6 +35,7 @@ int main(int argc, char *argv[]) {
 			afterNewLine = 0;
 			continue;
 		}
+		// previous character no longer a newline
 		afterNewLine = 0;
 		// remove tab and newline chars
 		if (ch == '\t') {
@@ -40,10 +46,12 @@ int main(int argc, char *argv[]) {
 			afterNewLine = 1;
 			continue;
 		}
-		all[i] = ch;
-		i = i + 1;
+		// place current char in the complete char[] of all input
+		all[current] = ch;
+		// then iterate
+		current = current + 1;
 	}
-	printf("\nArray length: %i", i - 1);
+	printf("\nTotal characters: %i", current - 1);
 	printf("\nCode: %s\n", all);
 	
 	// array is now set up, future expansion here
