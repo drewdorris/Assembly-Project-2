7a8c0b3304da34164c2b189c1527a980b46a097d
Drew <contact@dorrisd.com>
2021-10-20 10:47:05 -0400

    fix warnings, add defined limits

--- project.c
+++ project.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+
+#define MAX_FULL_LENGTH 2000
+#define MAX_LINE_LENGTH 50
+
 int main(int argc, char *argv[]) {
 	FILE *file = fopen(argv[1], "r"); // "r" for read
 	if (file == NULL) {
@@ -7,14 +11,14 @@ int main(int argc, char *argv[]) {
 		return 0;
 	}
 	
-	char all[2000]; // array containing all text from file
-	char buff[50]; // temp 
+	char all[MAX_FULL_LENGTH]; // array containing all text from file
+	char buff[MAX_LINE_LENGTH]; // temp 
 	int i = 0;
 	// https://stackoverflow.com/questions/16869467/command-line-arguments-reading-a-file
-	while(fgets(buff, 50 - 1, file) != NULL) 
+	while(fgets(buff, MAX_LINE_LENGTH - 1, file) != NULL) 
     {
 		int n = 0;
-		while (buff[n] != NULL) {
+		while (buff[n] != '\0') {
 			if (buff[n] == '\t') {
 				n = n + 1;
 				continue;
@@ -31,11 +35,11 @@ int main(int argc, char *argv[]) {
 	fclose(file);  /* close the file */ 
 	
 	i = 0;
-	while (all[i] != NULL) {
+	while (all[i] != '\0') {
 		printf("%c", all[i]);
 		i = i + 1;
 	}
-	printf("Array length: %i", i);
+	printf("\nArray length: %i\n", i);
 	
 	return 0;
 }
