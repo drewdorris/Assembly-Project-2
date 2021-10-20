8903b38dc13ea018fc65972edf3e9834039dd3bd
Drew <contact@dorrisd.com>
2021-10-20 14:26:55 -0400

    add comments, fix formatting and var names

--- project.c
+++ project.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX_FULL_LENGTH 2000
-#define MAX_LINE_LENGTH 50
+#define MAX_FULL_LENGTH 2000 // maximum amnt of characters in the file
+#define MAX_LINE_LENGTH 50 // maximum amnt of characters per line
 
 int main(int argc, char *argv[]) {
 	FILE *file = fopen(argv[1], "r"); // "r" for read
@@ -11,32 +11,39 @@ int main(int argc, char *argv[]) {
 		return 0;
 	}
 	
-	char all[MAX_FULL_LENGTH]; // array containing all text from file
-	char buff[MAX_LINE_LENGTH]; // temp 
-	int i = 0;
+	char all[MAX_FULL_LENGTH]; // array will contain all text from file
+	char buff[MAX_LINE_LENGTH]; // temp stores each line of the file
+	
+	int allIndex = 0;
 	// https://stackoverflow.com/questions/16869467/command-line-arguments-reading-a-file
-	while(fgets(buff, MAX_LINE_LENGTH - 1, file) != NULL) 
-    {
-		int n = 0;
-		while (buff[n] != '\0') {
-			if (buff[n] == '\t') {
-				n = n + 1;
+	// above link for retrieving data from file
+	// while loop loops through the char[] received from each line and adds it to the end of all[]
+	while (fgets(buff, MAX_LINE_LENGTH - 1, file) != NULL) {
+		int buffIndex = 0;
+		// individually add characters from buff to all
+		// \0 is null terminator. indicates end of line
+		while (buff[buffIndex] != '\0') {
+			// remove tabs
+			if (buff[buffIndex] == '\t') {
+				buffIndex = buffIndex + 1;
 				continue;
 			}
-			all[i] = buff[n];
-			i = i + 1;
-			n = n + 1;
+			all[allIndex] = buff[buffIndex];
+			allIndex = allIndex + 1;
+			buffIndex = buffIndex + 1;
 		}
-		i = i - 1; // remove null terminator at end
-    }
-	fclose(file);  /* close the file */ 
-	
-	i = 0;
-	while (all[i] != '\0') {
-		printf("%c", all[i]);
-		i = i + 1;
+		allIndex = allIndex - 1; // remove null terminator at end of each line
 	}
-	printf("\nArray length: %i\n", i);
+	fclose(file);  // close the file after using it
+	
+	int count = -1;
+	// count characters in all[]
+	while (all[count + 1] != '\0') {
+		count = count + 1;
+	}
+	// print array length, and the arrays contents as a string
+	printf("\nArray length: %i\n", count);
+	printf("%s", all);
 	
 	return 0;
 }
