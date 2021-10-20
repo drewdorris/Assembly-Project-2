5c57b04f9fc5fb0a1ccb1b2d3c75d62540bf2753
Drew <contact@dorrisd.com>
2021-10-20 19:15:17 -0400

    remove C directives while taking in input

--- project.c
+++ project.c
@@ -7,14 +7,37 @@
 int main(int argc, char *argv[]) {
 	char all[MAX_FILE_LENGTH];
 	
+	short afterNewLine = 0;
+	short ignoreLine = 0;
 	char ch;
 	int i = 0;
 	// https://stackoverflow.com/questions/15883568/reading-from-stdin
 	// iterate each char in input
 	while(read(STDIN_FILENO, &ch, 1) > 0)
 	{
+		// if we're ignoring this line and not on a new line, continue ignoring it
+		if (ignoreLine == 1 && ch != '\n') {
+			continue;
+		// stop ignoring once we hit a new line
+		} else if (ignoreLine == 1 && ch == '\n') {
+			ignoreLine = 0;
+			afterNewLine = 1;
+			continue;
+		}
+		// if we hit a C directive, we're ignoring this next line
+		if (ch == '#' && afterNewLine == 1) {
+			ignoreLine = 1;
+			afterNewLine = 0;
+			continue;
+		}
+		afterNewLine = 0;
 		// remove tab and newline chars
-		if (ch == '\t' || ch == '\n') {
+		if (ch == '\t') {
+			continue;
+		}
+		if (ch == '\n') {
+			// if a newline, keep track of that for when checking the next char
+			afterNewLine = 1;
 			continue;
 		}
 		all[i] = ch;
