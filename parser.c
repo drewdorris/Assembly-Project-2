ce1bf502e046466a83403f42894e3c2c11ab6e34
lwhitelaw <48133276+lwhitelaw@users.noreply.github.com>
2021-10-21 21:33:11 -0400

    Slight change to recursive-descent

--- parser.c
+++ parser.c
@@ -4,37 +4,30 @@
 #include "token.h"
 
 void parserInit(struct parser * self) {
-	int test = TYPE_WHITESPACE;
 	self->currentToken = 0;
-	self->stackPtr = 0;
-	for (int i = 0; i < PARSER_TOKEN_STACK_MAX; i++) {
-		tokenInit(&self->tokenStack[i]);
-	}
 }
 
-void parserShift(struct parser * self) {
-	struct token currToken = self->tokenArray[self->currentToken];
-	if (self->stackPtr == PARSER_TOKEN_STACK_MAX) {
-		fprintf(stderr,"Token stack limit exceeeded");
-		exit(1);
+int parserNext(struct parser * self) {
+	if (self->currentToken == self->tokenArrayLength) {
+		return 0;
 	}
-	self->tokenStack[self->stackPtr] = currToken;
-	self->stackPtr++;
 	self->currentToken++;
-}
-
-int parserElementNIs(struct parser * self, int n, int type) {
-	return 0;
-}
-
-struct token * parserElementN(struct parser * self, int n) {
-	return 0;
+	return 1;
 }
 
 int parserLookaheadIs(struct parser * self, int type) {
-	return 0;
+	return parserLookahead(self)->type == type;
 }
 
 struct token * parserLookahead(struct parser * self) {
-	return 0;
+	return self->tokenArray[self->currentToken];
+}
+
+void parserExpectOrError(struct parser * self, int type) {
+	if (parserLookaheadIs(parser,type)) {
+		parserNext(parser);
+	} else {
+		fprintf(stderr,"Syntax error");
+		exit(1);
+	}
 }
--- parser.h
+++ parser.h
@@ -2,27 +2,21 @@
 #define PARSER_H
 #include "token.h"
 
-#define PARSER_TOKEN_STACK_MAX 256
-
 struct parser {
 	//Input format not specified right now. Probably an array of tokens.
 	struct token * tokenArray;
 	int tokenArrayLength;
 	int currentToken;
-	struct token tokenStack[PARSER_TOKEN_STACK_MAX];
-	int stackPtr;
 };
 
 void parserInit(struct parser * self);
 
-void parserShift(struct parser * self);
-
-int parserElementNIs(struct parser * self, int n, int type);
-
-struct token * parserElementN(struct parser * self, int n);
+int parserNext(struct parser * self);
 
 int parserLookaheadIs(struct parser * self, int type);
 
 struct token * parserLookahead(struct parser * self);
+
+void parserExpectOrError(struct parser * self, int type);
 
 #endif
