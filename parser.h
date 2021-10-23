49157ec6c0d3c2876b1f011f5516615122727a90
lwhitelaw <48133276+lwhitelaw@users.noreply.github.com>
2021-10-22 20:45:45 -0400

    Finished node declarations

--- parser.h
+++ parser.h
@@ -35,7 +35,7 @@ void printfParse(struct parser * self);
 //Root node for a program containing a series of declarations, one of which is main
 struct program {
 	int nDeclarations;
-	struct declaration * array;
+	struct declaration * declarations;
 };
 
 //A declaration, which could be a variable or a function, especially the main function
@@ -48,25 +48,70 @@ struct declaration {
 
 	//Variable
 	int variableType; // may be TYPE_KW_SHORT or TYPE_KW_INT
-	struct expression * initialiser; // if present otherwise null e.g. "int x;" is a null pointer, "int x = 5;" has pointer to expression
+	struct expression * init; // if present otherwise null e.g. "int x;" is a null pointer, "int x = 5;" has pointer to expression
 
 	//Functions (MAIN ignores return type, identifier, and arguments. Only functionBlock is used.)
 	int returnType; // may be TYPE_KW_SHORT or TYPE_KW_INT or TYPE_KW_VOID
-	int nArgs; // number of arguments
+	int nArgTypes; // number of arguments
 	int * argTypes; // argument types, only TYPE_KW_SHORT or TYPE_KW_INT
 	struct block * functionBlock; // pointer to code block
 };
 
-//A set of {} and the statements within.
+//A set of {} and the declarations/statements within.
 struct block {
-	int nStatements;
-	struct statement * statements;
+	int nElements;
+	struct blockElement * elements;
+};
+
+//An element in a block. Either a declaration or statement.
+#define BLCK_DECLARATION 0
+#define BLCK_STATEMENT 1
+struct blockElement {
+	int type;
+	void * element; //must be casted to (struct statement *) or (struct declaration *) depending on type
 };
 
 //A statement in C, which is that stuff before the ;
-#define STMT_VAR_DECLARATION 1
+#define STMT_BLOCK 0 // A block is a grouping of statements/decls and is itself a statement as a whole
+#define STMT_VAR_ASSIGNMENT 1
+#define STMT_FUNCTION_CALL 2
+#define STMT_PRINTF_CALL 3
+#define STMT_SCANF_CALL 4
+#define STMT_RETURN 5
+#define STMT_IF 6 // Doesn't end with ; but is functionally the same
+#define STMT_WHILE 7 // Doesn't end with ; but is functionally the same
 struct statement {
+	int statementType;
+	//Block statements
+	struct block * blockElement;
+	//Types with identifiers (var assign, function call, scanf target)
+	char * identifer;
+	//Types with right-hand sides
+	struct expression * rhs; // Value of the variable assignment, return number, printf
+	//Function calls
+	//ignore for now- stretch goal
+};
 
+//An expression of variables, constants, and operators
+#define EXPR_VAL_UNARY 0 // Left side is alone. There is no right hand. Only unary operators are permitted. Not valid in left side.
+#define EXPR_VAL_NUMBER 1
+#define EXPR_VAL_STRING 2
+#define EXPR_VAL_IDENTIFIER 3 // Variable, hopefully
+#define EXPR_VAL_EXPRESSION 4
+
+#define EXPR_OP_NOP 0 //No operation. For constants defined alone i.e. "5"
+#define EXPR_OP_ADD 1
+#define EXPR_OP_SUB 2
+#define EXPR_OP_AND 3
+#define EXPR_OP_OR 4
+#define EXPR_OP_NEG 5 //Unary
+
+struct expression {
+	int leftType;
+	void * left; // must be casted to (struct expression *), (int *), or (char *)
+	int operator;
+	int rightType;
+	void * right; // must be casted to (struct expression *), (int *), or (char *)
 };
 
 #endif
