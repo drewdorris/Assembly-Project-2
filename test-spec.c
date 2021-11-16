4232c7bd62f923f7eeab0e010247bc813ce4db04
lwhitelaw <48133276+lwhitelaw@users.noreply.github.com>
2021-11-15 20:59:23 -0500

    Test spec

---
+++ test-spec.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int gVar1;
+int gVar2 = 42;
+
+int main() {
+	printf("Hello World!\n");
+
+	int x;
+	int y = 42;
+
+	scanf("%d",x);
+	printf("%d",x);
+
+	x = y + x;
+	x = x + 1;
+
+	printf("%d",x);
+
+	{
+		short a = 1;
+		short b = 2;
+		short c = a | b;
+		printf("%d",c);
+	}
+
+	{
+		gVar1 = 41;
+		gVar1 = gVar2 - gVar1;
+		gVar1 = ~gVar1;
+		printf("%d",gVar1);
+	}
+	printf("Thats all folks\n");
+	return 0;
+}
